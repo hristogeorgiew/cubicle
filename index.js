061dddc529c4a300689fb03602a9cb3b36420758
Hristo <h.georgiew93@gmail.com>
2021-02-12 16:13:35 +0200

    Add view angine and home page

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const config = require('./config/config');
+const handlebars = require('express-handlebars');
 
 const app = express();
 
-console.log(process.env.NODE_ENV);
+app.engine('hbs', handlebars({
+    extname: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {layout:false})
+});
 
 app.listen(config.PORT, () => console.log(`Server is running on port: ${config.PORT}...`));
