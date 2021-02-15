38c7dbd1c61ea34d98e9d647d8f806524854b466
Hristo <h.georgiew93@gmail.com>
2021-02-15 16:24:20 +0200

    Add details page

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,11 +6,12 @@ router.get('/', (req, res) => {
     res.render('home.hbs', {title: 'Browse'});
  });
 
- 
-
 router.get('/create', (req, res) => {
    res.render('create.hbs', {title: 'Create'});
 });
 
+router.get('/details/:productId', (req, res) => {
+    res.render('details.hbs', {title: 'Details'});
+});
 
 module.exports = router;
