fc793e90a1be237be1cb02a037ccf8d4afb837ce
Hristo <h.georgiew93@gmail.com>
2021-02-15 15:06:01 +0200

    Add layout to project

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,13 +3,13 @@ const router = Router();
 
 
 router.get('/', (req, res) => {
-    res.render('home', {layout:false});
+    res.render('home.hbs');
  });
 
  
 
 router.get('/create', (req, res) => {
-   res.render('create', {layout:false});
+   res.render('create.hbs');
 });
 
 
